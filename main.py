2441b926ea3ed0da51f7c254ea39e82b670d4607
GermanoAndrade <germano.andrade.br@gmail.com>
2023-12-17 01:03:42 -0300

    Add interface main page
    This is the main page of the interface, which contains the initial
    page and the sidebar configuration.

---
+++ main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from pages import newDrug, newVirus, sobre
+
+st.set_page_config(page_title= 'Projeto de TCC | EMAp - FGV', 
+                   page_icon="https://raw.githubusercontent.com/GermanoAndrade/AED-Listas/main/Lista%203/Quest%C3%B5es/Cap%C3%ADtulo%204/FGV-EMAp.png", 
+                   layout="wide",# 'centered' or 'wide'
+                   initial_sidebar_state="expanded",# 'auto', 'expanded' or 'collapsed'
+                   )
+
+PAGES = {'Nova Droga': newDrug,
+         'Novo Vírus': newVirus,
+         'Sobre'     : sobre}
+
+st.markdown(""" <style>
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+
+st.markdown("""
+    <style>
+        div[data-testid="stSidebarNav"] {visibility: hidden;}
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown(""" <style>
+.stDeployButton {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+
+st.sidebar.markdown("---")
+st.sidebar.header("Reposicionamento de Drogas")
+st.sidebar.markdown("Projeto de TCC - 2023 | EMAp - FGV")
+st.write('<style>div.row-widget.stRadio > div{flex-direction:column;}</style>', unsafe_allow_html=True)
+
+option = st.sidebar.radio('Ir para:', options=list(PAGES.keys()), index=0)
+page = PAGES[option]
+st.sidebar.markdown("""
+---
+
+Germano Andrade | Dezembro, 2023  
+[<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg" width="30px"> Github](https://github.com/GermanoAndrade/)
+
+""", unsafe_allow_html=True)
+#init.begin_variables()
+
+page.app()
+
+
+
+
